vtokarev/khtml-soc09

commit 2b236fff60dccd4fbc78d4a071bb89fe9f3160ab

Don't enforce "Default" view on the diroperator as it breaks reading from the configuration and also makes it necessary to do hacks in apps that use KFileWidget and want a different view mode.
git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/KDE/kdelibs@981593 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

kfile/kfilewidget.cpp
@@ -1712,7 +1712,6 @@ void KFileWidget::showEvent(QShowEvent* event)
     if ( !d->hasView ) { // delayed view-creation
         Q_ASSERT( d );
         Q_ASSERT( d->ops );
-        d->ops->setView( KFile::Default );
         d->ops->view()->setSizePolicy( QSizePolicy( QSizePolicy::Maximum, QSizePolicy::Maximum ) );
         d->hasView = true;
 

commit 5e336556777522340745d5ddc18e80743fd5759f

Fix the crash I introduced with r981593, who knew that a completely innocent method like "setView" actually creates the view behind the scenes and is needed to be called here. Wonder what the author of this design was smoking at the time.
git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/KDE/kdelibs@981652 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

kfile/kdiroperator.cpp
@@ -2627,6 +2627,11 @@ void KDirOperator::setDecorationPosition(QStyleOptionViewItem::Position position
     d->actionCollection->action("decorationAtTop")->setChecked(!decorationAtLeft);
 }
 
+KFile::FileView KDirOperator::viewKind() const
+{
+    return static_cast<KFile::FileView>(d->viewKind);
+}
+
 // ### temporary code
 #include <dirent.h>
 bool KDirOperator::Private::isReadable(const KUrl& url)

kfile/kdiroperator.h
@@ -852,6 +852,13 @@ Q_SIGNALS:
      */
     void currentIconSizeChanged(int size);
 
+public:
+    /**
+     * @returns the currently used view kind
+     * @see setView
+     */
+    KFile::FileView viewKind() const;
+
 private:
     class Private;
     Private* const d;

kfile/kfilewidget.cpp
@@ -1712,6 +1712,7 @@ void KFileWidget::showEvent(QShowEvent* event)
     if ( !d->hasView ) { // delayed view-creation
         Q_ASSERT( d );
         Q_ASSERT( d->ops );
+        d->ops->setView( d->ops->viewKind() );
         d->ops->view()->setSizePolicy( QSizePolicy( QSizePolicy::Maximum, QSizePolicy::Maximum ) );
         d->hasView = true;
 