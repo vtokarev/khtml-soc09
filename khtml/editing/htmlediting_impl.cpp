5b92736c2becc4b26254987a4396ce5fc89b6803
vtokarev <tsjoker@gmail.com>
2009-07-26 16:04:03 +0700

    don't insert non-breaking spaces in the middle of the line it's ok to break it there

--- khtml/editing/htmlediting_impl.cpp
+++ khtml/editing/htmlediting_impl.cpp
@@ -1887,6 +1887,11 @@ void InputTextCommandImpl::insertSpace(TextImpl *textNode, unsigned long offset)
         return;
     }
 
+    if (!Position(textNode, offset).isLastRenderedPositionOnLine()) {
+        insertText(textNode, offset, " ");
+        return;
+    }
+
     // insert an nbsp
     insertText(textNode, offset, nonBreakingSpaceString());
 }
